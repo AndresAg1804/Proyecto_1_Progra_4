9fa6312e16edc377fc5d9b885f71d51104709184
Nacho <ignacio.bonilla.rojas@est.una.ac.cr>
2024-03-28 22:41:07 -0600

    Funcionamiento correcto de las hojas de estilo

--- src/main/java/org/example/sistemaproveedores/presentation/Login/Controller.java
+++ src/main/java/org/example/sistemaproveedores/presentation/Login/Controller.java
@@ -1,11 +1,16 @@
 package org.example.sistemaproveedores.presentation.Login;
 import org.example.sistemaproveedores.logic.Service;
-import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller("login")
 public class Controller {
 
+    @Autowired
+    private Service service;
 
+    @GetMapping("/presentation/Login/show")
+    public String show() {
+        return "Presentation/Login/view";
+    }
 
 }
