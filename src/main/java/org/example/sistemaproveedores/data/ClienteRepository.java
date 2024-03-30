5cfc9924f01c2edf0a75f508029572db840347d6
Nacho <ignacio.bonilla.rojas@est.una.ac.cr>
2024-03-30 11:59:16 -0600

    Avances en html, css y repository de clientes

--- src/main/java/org/example/sistemaproveedores/data/ClienteRepository.java
+++ src/main/java/org/example/sistemaproveedores/data/ClienteRepository.java
@@ -1,9 +1,18 @@
 package org.example.sistemaproveedores.data;
 
 import org.example.sistemaproveedores.logic.Clientes;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends CrudRepository<Clientes, String> {
+
+    @Query("select c from Clientes c where c.proveedoresByProveedorid.idP=?1")
+    List<Clientes> findClientesByProveedor(String idProveedor);
+
+
+
 }
