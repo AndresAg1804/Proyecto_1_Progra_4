aae77b8a2c7c83e4e3956a6d4481fb58023324f8
Nacho <ignacio.bonilla.rojas@est.una.ac.cr>
2024-04-01 23:17:39 -0600

    Avance en facturación

---
+++ src/main/java/org/example/sistemaproveedores/data/ProductosRepository.java
@@ -0,0 +1,12 @@
+package org.example.sistemaproveedores.data;
+
+
+import org.example.sistemaproveedores.logic.Proveedores;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.example.sistemaproveedores.logic.Producto;
+@Repository
+public interface ProductosRepository extends CrudRepository<Producto, String>{
+
+    Producto findByIdPrAndProveedoresByIdProd(String idProducto, Proveedores proveedor);
+}
