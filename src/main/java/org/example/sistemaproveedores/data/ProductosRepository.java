e2e419275ec72e67d26821d5b6fb69a95490ece4
jpce <jpcartinvillalobos@outlook.com>
2024-04-09 03:55:04 -0600

    ultima del dia

--- src/main/java/org/example/sistemaproveedores/data/ProductosRepository.java
+++ src/main/java/org/example/sistemaproveedores/data/ProductosRepository.java
@@ -1,13 +1,20 @@
 package org.example.sistemaproveedores.data;
 
 
+import org.example.sistemaproveedores.logic.Clientes;
 import org.example.sistemaproveedores.logic.Proveedores;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.example.sistemaproveedores.logic.Producto;
+
+import java.util.List;
+
 @Repository
 public interface ProductosRepository extends CrudRepository<Producto, String>{
 
     Producto findByIdPrAndProveedoresByIdProd(String idProducto, Proveedores proveedor);
-    //Producto findProductoByProveedoresByIdProd(String idProvedor);
+    @Query("select p from Producto p where p.proveedoresByIdProd.idP=?1")
+    List<Producto> get_all_productos_de_IDprovedor(String idProveedor);
+
 }
