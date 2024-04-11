0684a6a5aab29a27171082c89456dde50359f6d5
Nacho <ignacio.bonilla.rojas@est.una.ac.cr>
2024-04-11 00:18:06 -0600

    Facturar finalizado y funcional

--- src/main/java/org/example/sistemaproveedores/data/DetalleRepository.java
+++ src/main/java/org/example/sistemaproveedores/data/DetalleRepository.java
@@ -2,9 +2,17 @@ package org.example.sistemaproveedores.data;
 
 import org.example.sistemaproveedores.logic.Detalle;
 import org.example.sistemaproveedores.logic.Facturas;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface DetalleRepository extends CrudRepository<Detalle, Integer> {
+
+    @Query("select c from Detalle c")
+    public List<Detalle> findAll();
 }
